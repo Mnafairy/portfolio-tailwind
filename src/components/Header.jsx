7eae58c246d530f5bff3211ee2eb5763c82af6db
Orgil <mnafairy@gmail.com>
2024-01-22 21:06:26 +0800

    a

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./Navbar";
+import { SunIcon } from "./icons/SunIcon";
 export const Header = () => {
   return (
     <div className="flex flex-row justify-between px-4 py-4 sm:px-[80px] ">
@@ -14,78 +15,48 @@ export const Header = () => {
           <div className="w-px text-gray-100 h-6	">I</div>
         </div>
         <button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-          >
-            <path
-              d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"
-              stroke="#4B5563"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M12 2V4"
-              stroke="#4B5563"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M12 20V22"
-              stroke="#4B5563"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M4.92993 4.92999L6.33993 6.33999"
-              stroke="#4B5563"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M17.6599 17.66L19.0699 19.07"
-              stroke="#4B5563"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M2 12H4"
-              stroke="#4B5563"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M20 12H22"
-              stroke="#4B5563"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M6.33993 17.66L4.92993 19.07"
-              stroke="#4B5563"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M19.0699 4.92999L17.6599 6.33999"
-              stroke="#4B5563"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
+          <SunIcon />
         </button>
+        <div class="flex flex-col justify-center ml-3">
+          <input
+            type="checkbox"
+            name="light-switch"
+            class="light-switch sr-only"
+          />
+          <label class="relative cursor-pointer p-2" for="light-switch">
+            <svg
+              class="dark:hidden"
+              width="16"
+              height="16"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                class="fill-slate-300"
+                d="M7 0h2v2H7zM12.88 1.637l1.414 1.415-1.415 1.413-1.413-1.414zM14 7h2v2h-2zM12.95 14.433l-1.414-1.413 1.413-1.415 1.415 1.414zM7 14h2v2H7zM2.98 14.364l-1.413-1.415 1.414-1.414 1.414 1.415zM0 7h2v2H0zM3.05 1.706 4.463 3.12 3.05 4.535 1.636 3.12z"
+              />
+              <path
+                class="fill-slate-400"
+                d="M8 4C5.8 4 4 5.8 4 8s1.8 4 4 4 4-1.8 4-4-1.8-4-4-4Z"
+              />
+            </svg>
+            <svg
+              class="hidden dark:block"
+              width="16"
+              height="16"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                class="fill-slate-400"
+                d="M6.2 1C3.2 1.8 1 4.6 1 7.9 1 11.8 4.2 15 8.1 15c3.3 0 6-2.2 6.9-5.2C9.7 11.2 4.8 6.3 6.2 1Z"
+              />
+              <path
+                class="fill-slate-500"
+                d="M12.5 5a.625.625 0 0 1-.625-.625 1.252 1.252 0 0 0-1.25-1.25.625.625 0 1 1 0-1.25 1.252 1.252 0 0 0 1.25-1.25.625.625 0 1 1 1.25 0c.001.69.56 1.249 1.25 1.25a.625.625 0 1 1 0 1.25c-.69.001-1.249.56-1.25 1.25A.625.625 0 0 1 12.5 5Z"
+              />
+            </svg>
+            <span class="sr-only">Switch to light / dark version</span>
+          </label>
+        </div>
         <button className="px-4 py-1.5 bg-gray-900 text-white rounded-xl ">
           Download CV
         </button>
